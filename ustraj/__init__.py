31e6e15c3c6e2286383d3b5e6e5009bd11393f95
BenTanYK <tanyk849@gmail.com>
2026-02-25 19:30:15 +0000

    Flatten API

--- ustraj/__init__.py
+++ ustraj/__init__.py
@@ -1,10 +1,12 @@
-"""A collection of scripts for analysing trajectory files collected during US simulations"""
+"""A collection of scripts for analysing trajectory files
+collected during US simulations"""
 
 import importlib.metadata
+from . import analysis
 
 try:
     __version__ = importlib.metadata.version("ustraj")
 except importlib.metadata.PackageNotFoundError:  # pragma: no cover
     __version__ = "0+unknown"
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "analysis"]
